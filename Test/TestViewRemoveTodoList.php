f3953a718ec546a49a261dbabcf419038e9c8381
Aisyah_Itsnaini <MY_NAME@example.com>
2024-12-10 17:32:04 +0700

    bismillah

--- Test/TestViewRemoveTodoList.php
+++ Test/TestViewRemoveTodoList.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "../Model/TodoList.php";
-require_once "../View/ViewTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
 require_once "../BusinessLogic/AddTodoList.php";
 require_once "../BusinessLogic/ShowTodoList.php";
 
