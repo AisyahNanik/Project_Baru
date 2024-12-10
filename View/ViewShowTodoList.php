f6a0b16fea2b12effbbe395ae9f3813d25b707c6
Aisyah_Itsnaini <MY_NAME@example.com>
2024-12-10 15:47:52 +0700

    bismillah

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../Model/TodoList.php";
 require_once __DIR__ . "/../BusinessLogic/ShowtodoList.php";
 require_once __DIR__ . "/../View/ViewAddTodoList.php";
 require_once __DIR__ . "/../View/ViewRemoveTodoList.php";
-require_once __DIR__ . "/.. /Helper/Input.php";
+require_once __DIR__ . "/../Helper/Input.php";
 
 function viewShowTodoList()
 {
